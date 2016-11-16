83f61589bd329816cae9df080bb18cd883b4cabd
Devon <Sanitygamers@outlook.com>
2016-11-15 22:26:25 -0600

    Made sure all names are striked as lowercase

--- EconomyBot/EconomyBot.cs
+++ EconomyBot/EconomyBot.cs
@@ -9,10 +9,10 @@ namespace EconomyBot
 {
     class MyBot
     {
-        Dictionary<String, double> userEcon = new Dictionary<String, double>();
+        Dictionary<String, int> userEcon = new Dictionary<String, int>();
 
-        public Boolean Taken = false;       
-       
+        public Boolean Taken = false;
+
         String Args;
         Random random = new Random();
         int Amount;
@@ -21,7 +21,7 @@ namespace EconomyBot
         DiscordClient discord;
         public MyBot()
         {
-            userEcon.Add("server", 1000000.0);
+            userEcon.Add("server", 1000000);
             discord = new DiscordClient(x =>
             {
                 x.LogLevel = LogSeverity.Info;
@@ -43,10 +43,11 @@ namespace EconomyBot
                 .Do(async (e) =>
                 {
 
-                    if (Int32.Parse(e.GetArg("Amount")) < userEcon[e.User.Name]) {
-                        Amount = userEcon[e.User.Name.ToLowercase()] -= Int32.Parse(e.GetArg("Amount"));
-                        userEcon.Remove(e.User.Name.ToLowercase());
-                        userEcon.Add(e.User.Name.ToLowercase(), Amount);
+                    if (Int32.Parse(e.GetArg("Amount")) < userEcon[e.User.Name])
+                    {
+                        Amount = userEcon[e.User.Name.ToLower()] -= Int32.Parse(e.GetArg("Amount"));
+                        userEcon.Remove(e.User.Name.ToLower());
+                        userEcon.Add(e.User.Name.ToLower(), Amount);
                         await e.Channel.SendMessage("User " + e.User.Name + " Has bet " + e.GetArg("Amount") + " Virginities on the number " + e.GetArg("onWho"));
                         int randomNumber = random.Next(1, 7);
 
@@ -55,21 +56,22 @@ namespace EconomyBot
                         if (Int32.Parse(e.GetArg("onWho")) == randomNumber)
                         {
                             await e.Channel.SendMessage(e.User.Name + " has won " + (Int32.Parse(e.GetArg("Amount")) * 2));
-                            Amount = userEcon[e.User.Name.ToLowercase()] += (Int32.Parse(e.GetArg("Amount")) * 2);
-                            userEcon.Remove(e.User.Name.ToLowercase());
-                            userEcon.Add(e.User.Name.ToLowercase(), Amount);
-                        } else
+                            Amount = userEcon[e.User.Name.ToLower()] += (Int32.Parse(e.GetArg("Amount")) * 2);
+                            userEcon.Remove(e.User.Name.ToLower());
+                            userEcon.Add(e.User.Name.ToLower(), Amount);
+                        }
+                        else
                         {
                             await e.Channel.SendMessage("The die has rolled a " + randomNumber + " " + e.User.Name + " Has Lost " + Int32.Parse(e.GetArg("Amount")));
-                            Amount = userEcon[e.User.Name.ToLowercase()] -= Int32.Parse(e.GetArg("Amount"));
-                            userEcon.Remove(e.User.Name.ToLowercase());
-                            userEcon.Add(e.User.Name.ToLowercase(), Amount);
+                            Amount = userEcon[e.User.Name.ToLower()] -= Int32.Parse(e.GetArg("Amount"));
+                            userEcon.Remove(e.User.Name.ToLower());
+                            userEcon.Add(e.User.Name.ToLower(), Amount);
                         }
 
                     }
                 });
 
-          
+
             commands.CreateCommand("pay")
                .Parameter("TargetUser", ParameterType.Required)
                .Parameter("Amount", ParameterType.Required)
@@ -81,30 +83,33 @@ namespace EconomyBot
                    if (userEcon.ContainsKey(Args))
                    {
 
-                   } else
-                   {
-                       userEcon.Add(Args, 100.0);
                    }
-                   if(!userEcon.ContainsKey(e.User.Name))
+                   else
+                   {
+                       userEcon.Add(Args, 100);
+                   }
+                   if (!userEcon.ContainsKey(e.User.Name.ToLower()))
                    {
                        userEcon.Add(e.User.Name, 100);
                    }
-                   if(userEcon[e.User.Name] > Int32.Parse(e.GetArg("Amount")))
+                   if (userEcon[e.User.Name.ToLower()] > Int32.Parse(e.GetArg("Amount")))
                    {
                        Allowed = true;
-                   } else
+                   }
+                   else
                    {
                        Allowed = false;
                        await e.Channel.SendMessage("User: " + e.User.Name + " does not have enough virginities to pay " + e.GetArg("TargetUser") + " " + e.GetArg("Amount") + " Virginities");
                    }
 
-                   while (Allowed == true) {
-                       Amount = userEcon[e.User.Name] -= Int32.Parse(e.GetArg("Amount"));
-                       Amount2 = userEcon[Args] += Int32.Parse(e.GetArg("Amount"));
-                       userEcon.Remove(Args);
-                       userEcon.Remove(e.User.Name);
-                       userEcon.Add(e.User.Name, Amount);
-                       userEcon.Add(Args, Amount2);
+                   while (Allowed == true)
+                   {
+                       Amount = userEcon[e.User.Name.ToLower()] -= Int32.Parse(e.GetArg("Amount"));
+                       Amount2 = userEcon[Args.ToLower()] += Int32.Parse(e.GetArg("Amount"));
+                       userEcon.Remove(Args.ToLower());
+                       userEcon.Remove(e.User.Name.ToLower());
+                       userEcon.Add(e.User.Name.ToLower(), Amount);
+                       userEcon.Add(Args.ToLower(), Amount2);
 
                        await e.Channel.SendMessage("[Test 7]" + e.User.Name + " has paid " + e.GetArg("TargetUser") + " " + e.GetArg("Amount") + " Virginities");
                        //Modifier(e.User.Name, e.GetArg("TargetUser"), Int32.Parse(e.GetArg("Amount")));
@@ -124,28 +129,30 @@ namespace EconomyBot
                 });
 
             commands.CreateCommand("truth")
-                .Do(async (e) => 
-                 {
+                .Do(async (e) =>
+                {
                     await e.Channel.SendMessage("Jack's a retard");
 
-                 });
+                });
 
             commands.CreateCommand("welfare")
                .Do(async (e) =>
                {
-                   if (Taken == false) {
+                   if (Taken == false)
+                   {
 
-                       if (!(userEcon.ContainsKey(e.User.Name)))
+                       if (!(userEcon.ContainsKey(e.User.Name.ToLower())))
                        {
-                           userEcon.Add(e.User.Name, 100.0);
+                           userEcon.Add(e.User.Name.ToLower(), 100);
                        }
 
-                       Amount = userEcon[e.User.Name] += 1500;
+                       Amount = userEcon[e.User.Name.ToLower()] += 1500;
                        await e.Channel.SendMessage(e.User.Name + " has just got da mfin welfare check of 1500 virginities");
 
                        Taken = true;
-                       
-                   } else if(Taken == true)
+
+                   }
+                   else if (Taken == true)
                    {
                        await e.Channel.SendMessage("The welfare check is not ready!");
                    }
@@ -156,41 +163,43 @@ namespace EconomyBot
                 {
                     Taken = true;
                     await e.Channel.SendMessage("Welfare is now added");
-                    
+
 
                 });
-         
+
             commands.CreateCommand("eggplantme")
-                
+
                 .Do(async (e) =>
                 {
-                   // for (int x = 0; x < Int32.Parse("Amount"); x++) {
-                        await e.Channel.SendMessage(":eggplant:");
+                    // for (int x = 0; x < Int32.Parse("Amount"); x++) {
+                    await e.Channel.SendMessage(":eggplant:");
                     //}
                 });
             commands.CreateCommand("bank")
                 .Parameter("User", ParameterType.Optional)
                 .Do(async (e) =>
                 {
-                    if (!(userEcon.ContainsKey(e.User.Name)))
-                    { 
-                        userEcon.Add(e.User.Name, 100.0);
-                    }
-                    if (!(userEcon.ContainsKey(e.GetArg("User")))) 
+                    if (!(userEcon.ContainsKey(e.User.Name.ToLower())))
                     {
-                        userEcon.Add(e.GetArg("User"), 100.0);
+                        userEcon.Add(e.User.Name.ToLower(), 100);
+                    }
+                    if (!(userEcon.ContainsKey(e.GetArg("User").ToLower())))
+                    {
+                        userEcon.Add(e.GetArg("User").ToLower(), 100);
                     }
 
-                    if (e.GetArg("User").Equals(" ") || e.GetArg("User").Equals("")) {
-                        await e.Channel.SendMessage(e.User.Name + " You have " + userEcon[e.User.Name] + " Virginities");
-                    } else
+                    if (e.GetArg("User").Equals(" ") || e.GetArg("User").Equals(""))
                     {
-                        await e.Channel.SendMessage(e.GetArg("User") + " Has " + userEcon[e.GetArg("User")] + " Virginities");
+                        await e.Channel.SendMessage(e.User.Name + " You have " + userEcon[e.User.Name.ToLower()] + " Virginities");
+                    }
+                    else
+                    {
+                        await e.Channel.SendMessage(e.GetArg("User") + " Has " + userEcon[e.GetArg("User").ToLower()] + " Virginities");
                     }
 
                 });
 
-           
+
 
             discord.ExecuteAndWait(async () =>
             {
@@ -201,15 +210,6 @@ namespace EconomyBot
         private void Log(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine(e.Message);
-        }
-
-        public void Modifier(String InputName, String TargetUser, int money)
-        {
-            Amount = userEcon[TargetUser];
-            Amount2 = userEcon[InputName];
-
-            userEcon[TargetUser] = Amount2 + money;
-            userEcon[InputName] = Amount2 - money;
         }
     }
 }
