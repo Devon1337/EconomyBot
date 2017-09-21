cb1c0bc0518ea84fc7f69a11ee144ee4d8ac9876
Devon <Sanitygamers@outlook.com>
2017-09-21 09:37:35 -0500

    Create Logging.cs

---
+++ EconomyBot/ClassFiles/Logging.cs
@@ -0,0 +1,30 @@
+using System.*;
+
+namespace EconomyBot {
+
+    public Logging {
+         public int OutputFile() {
+         
+         return 0;
+         }
+         public int SetupOutputFile() {
+         
+         return 0;
+         }
+         /*
+         * PARAM
+         * getInformation("Name", UsersIp[], 1124, nameRank)
+         * ||
+         * getInformation(0.0.0.0, 1124, IPRank)
+         */
+         public int getInformation(string Name, int[] IPOcttet, int PortListener, string LocalRank) {
+         
+         return 0;
+         }
+         public int setInformation() {
+         
+         return 0;
+         }
+    }
+
+}
