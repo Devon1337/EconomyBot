99fa05526db4f5f0c5024e2a9e7adb567e1eaf81
Devon <Sanitygamers@outlook.com>
2017-09-27 11:54:52 -0500

    Update and rename EconomyBotServer to EconomyBotServer.cs

---
+++ EconomyBot/ClassFiles/EconomyBotServer.cs
@@ -0,0 +1,16 @@
+using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace EconomyBot {
+
+  public EconBotServer {
+  
+  
+  
+  }
+
+}
